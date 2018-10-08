c8dd8c7166b4479197f866d226fbd7f97feaca53
Omer Zauber Home <th3wd@yahoo.com>
2018-10-09 01:08:44 +0300

    add Logout menu and simple nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,38 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { Platform, MenuController } from "ionic-angular";
 // import { StatusBar } from '@ionic-native/status-bar';
 // import { SplashScreen } from '@ionic-native/splash-screen';
-import firebase from 'firebase';
+import firebase from "firebase";
 
-import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
-import { AuthService } from '../services/auth';
+import { HomePage } from "../pages/home/home";
+import { LoginPage } from "../pages/login/login";
+import { AuthService } from "../services/auth";
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html"
 })
 export class MyApp {
   rootPage: any = LoginPage;
+  // isAuthenticated = false;
 
-  constructor(platform: Platform, private authService: AuthService) {
+  constructor(platform: Platform, private authService: AuthService, private menuCtrl:MenuController) {
     firebase.initializeApp({
-      apiKey: 'AIzaSyBrblsIgjjQ6pwqRJ5sJzMY7_MdpOvIu_0',
-      authDomain: 'orders-app-d8da1.firebaseapp.com'
+      apiKey: "AIzaSyBrblsIgjjQ6pwqRJ5sJzMY7_MdpOvIu_0",
+      authDomain: "orders-app-d8da1.firebaseapp.com"
     });
-    // firebase.auth().onAuthStateChanged(user => {
-    //   if (user) {
-    //     this.isAuthenticated = true;
-    //     this.rootPage = TabsPage;
-    //   } else {
-    //     this.isAuthenticated = false;
-    //     this.rootPage = SigninPage;
-    //   }
-    // });
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        // this.isAuthenticated = true;
+        console.log(user);
+        this.rootPage = HomePage;
+      } else {
+        // this.isAuthenticated = false;
+        this.rootPage = LoginPage;
+      }
+    });
+  }
 
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      // statusBar.styleDefault();
-      // splashScreen.hide();
-    });
+  onLogout() {
+    this.authService.logout();
+    this.menuCtrl.close();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-// import { SplashScreen } from '@ionic-native/splash-screen';
-// import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -19,7 +17,8 @@ import { AuthService } from '../services/auth';
     LoginPage,
     OrderDetailsPage,
     OrdersPage,
-    SignupPage
+    SignupPage,
+    
   ],
   imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
